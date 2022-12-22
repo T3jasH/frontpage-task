6498d08bc5b5285c5cbc51b997d5b322054ec026
Tejas Hegde <hegdetejasn@gmail.com>
2022-12-22 15:53:12 +0530

    Added hover effect and latest message time in chats, removed dummy redux code

--- src/components/channels/Channel.tsx
+++ src/components/channels/Channel.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { selectChannel, selectCurrentChannel, setCurrentChannel} from '../../slices/channels';
 import { selectLatestMessageByChannelId } from '../../slices/messages';
@@ -8,21 +9,38 @@ const Channel = ({id}: {id: number}) => {
     const channel = useAppSelector(selectChannel(id))
     const latestMessage = useAppSelector(selectLatestMessageByChannelId(id))
     const selectedChannel = useAppSelector(selectCurrentChannel)
+    const [hovering, setHovering] = useState<boolean>(false)
     
     return <div 
-        className={`${style.channel} ${selectedChannel?.id === id ? style.selected : ''}`}
+        className={`${style.channel} ${selectedChannel?.id === id ? style.selected : hovering === true ? style.hovering : '' }`}
         onClick={() => dispatch(setCurrentChannel(id))}
+        onMouseEnter={() => {
+            if(selectedChannel?.id !== id){
+                setHovering(true)
+            }
+        }}
+        onMouseLeave={() => {
+                setHovering(false)
+        }}
         >
         <img
         alt='Display Pic'
         src={channel?.profileUrl}
         className={style.displayPic}
         />
-        <div className={style.chatContent}>
-            <p className={style.name}>{channel?.name}</p>
+        <div className={style.chat}>
+            <div className={style.chatHeader}>
+                <p className={style.name}>{channel?.name}</p>
+                <p className={style.time}>
+                {latestMessage && new Intl.DateTimeFormat('en-IN', {
+                    hour: 'numeric',
+                    minute: 'numeric',
+                }).format(new Date(latestMessage.receivedTime))}
+                </p>
+            </div>
             <p className={style.latestMessage} >
-                {latestMessage?.text}
-            </p>
+                    {latestMessage?.text}
+                </p>
         </div>
     </div>
 }
